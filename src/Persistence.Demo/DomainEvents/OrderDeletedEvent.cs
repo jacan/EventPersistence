67263000add1522ca4594ab76727422032a9c877
Jack Andersen <jckandersen@gmail.com>
2020-11-18 12:47:18 +0100

    Add OrderDeletedEvent

---
+++ src/Persistence.Demo/DomainEvents/OrderDeletedEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Persisted.Events;
+
+namespace Persistence.Demo.DomainEvents
+{
+    public class OrderDeletedEvent : IEvent
+    {
+        public Guid OrderId { get; }
+        public DateTime Timestamp { get; }
+
+        public OrderDeletedEvent(Guid orderId, DateTime timestamp)
+        {
+            OrderId = orderId;
+            Timestamp = timestamp;
+        }
+    }
+}
